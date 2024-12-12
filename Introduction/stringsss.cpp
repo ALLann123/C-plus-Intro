4461c2f98fd585d069d5663250b1a22eaf5f4bd5
ALLann123 <karisallan237@gmail.com>
2024-12-12 12:36:13 +0300

    strings c++

---
+++ Introduction/stringsss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name, dob, id_no;
+	cout<<"Enter Name: ";
+	cin>>name;
+	cout<<endl<<"DOB: ";
+	cin>>dob;
+	cout<<endl<<"ID: ";
+	cin>>id_no;
+	cout<<endl<<"Name: "<<name;
+	cout<<endl<<"DOB: "<<dob;
+	cout<<endl<<"ID: "<<id_no;
+	return 0;
+}
